eab808bd1eedf3d8458988f15330cc2540647814
amira <101064972+ajroudamira@users.noreply.github.com>
2024-02-15 22:24:30 +0100

    add iservice for everyone

---
+++ src/main/java/service/IService.java
@@ -0,0 +1,9 @@
+package service;
+
+import java.util.List;
+public interface IService<T> {
+    void ajouter(T t) ;
+    void modifier(int id,T t);
+    void supprimer(int id);
+    List<T> afficher();
+}
